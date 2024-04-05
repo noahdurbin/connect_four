8ed17d087fadedf49e45b2af9f85fb5acdb55c51
Noah Durbin <noahdurbin@gmail.com>
2024-04-04 21:26:17 -0600

    test: content for board tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,14 +7,14 @@ RSpec describe Board do
     end
 
     it 'is an instance of the board class' do
-
+        expect(board_1).to be_a(Board)
     end
 
-    it 'has 42 cells' do
-
+    it 'initializes with 42 cells' do
+        expect(board_1.cell_count).to eq(42)
     end
 
     it 'only contains unplayed cells' do
-
+        expect(board_1.cells.status).to all eq(".")
     end
 end
