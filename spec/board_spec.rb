520e0bc1fe1b8be9a3996da095a9424e2c8799de
Noah Durbin <noahdurbin@gmail.com>
2024-04-04 21:19:36 -0600

    test: added more tests for board and spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
 RSpec describe Board do
+    
     before do 
         @board_1 = Board.new
     end
 
     it 'is an instance of the board class' do
+
+    end
+
+    it 'has 42 cells' do
+
+    end
+
+    it 'only contains unplayed cells' do
 
     end
 end
--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 RSpec describe Cell do
 
+    it 'is a cell' do
+
+    end
+
+    it 'starts as an unplayed cell' do
+
+    end
+
 end
