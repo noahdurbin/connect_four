edac884769190d7b4138453f309ea52ec8af221f
Noah Durbin <noahdurbin@gmail.com>
2024-04-08 15:09:13 -0600

    test: check to see if computer can win on vertical win

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,13 +7,23 @@ RSpec.describe Game do
         expect(game_1).to be_a(Game)
     end
 
-    it "can determine if there is a winner" do
+    it "can determine if the player won is a winner" do
         game = Game.new
         game.player.place_piece("A")
         game.player.place_piece("A")
         game.player.place_piece("A")
         game.player.place_piece("A")
 
-        expect(game.check_columns).to eq("player wins")    
+        expect(game.check_columns).to eq("Game Over Player Wins")    
+    end
+
+    it 'can tell if the computer won the game' do 
+        game = Game.new
+        game.computer.place_piece("B")
+        game.computer.place_piece("B")
+        game.computer.place_piece("B")
+        game.computer.place_piece("B")
+
+        expect(game.check_columns).to eq("Game Over Computer Wins")
     end
 end
