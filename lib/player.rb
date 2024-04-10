672210767989a61e7349d95cf004d290952c060b
Noah Durbin <noahdurbin@gmail.com>
2024-04-09 20:00:17 -0600

    feat: worked on gameplay

--- lib/player.rb
+++ lib/player.rb
@@ -7,36 +7,37 @@ class Player
 
   def place_piece(column)
     if column == "A" && player_check(@board.a_column) == true
+      # binding.pry
       @board.a_column.find do |cell|
         cell.change_state("X")
         @flag += 1
       end
-    elsif column == "B" && player_check(@board.b_column) == true
+    elsif column == "B" && player_check(@board.a_column) == true
       @board.b_column.find do |cell|
         cell.change_state("X")
         @flag += 1
       end
-    elsif column == "C" && player_check(@board.c_column) == true
+    elsif column == "C" && player_check(@board.a_column) == true
       @board.c_column.find do |cell|
         cell.change_state("X")
         @flag += 1
       end
-    elsif column == "D" && player_check(@board.d_column) == true
+    elsif column == "D" && player_check(@board.a_column) == true
       @board.d_column.find do |cell|
         cell.change_state("X")
         @flag += 1
       end
-    elsif column == "E" && player_check(@board.e_column) == true
+    elsif column == "E" && player_check(@board.a_column) == true
       @board.e_column.find do |cell|
         cell.change_state("X")
         @flag += 1
       end
-    elsif column == "F" && player_check(@board.f_column) == true
+    elsif column == "F" && player_check(@board.a_column) == true
       @board.f_column.find do |cell|
         cell.change_state("X")
         @flag += 1
       end
-    elsif column == "G" && player_check(@board.g_column) == true
+    elsif column == "G" && player_check(@board.a_column) == true
       @board.g_column.find do |cell|
         cell.change_state("X")
         @flag += 1
@@ -48,17 +49,17 @@ class Player
 
   def player_check(full_column)
     full_column.map do |column|
-        cell_states = []
-        full_column.each do |cell|
-          cell_states.push(cell.state)
-        end
-        if cell_states.include?(".")
-          return true
-        else
-          print "Column full. Make another selection: "
-          return false
-        end
+      cell_states = []
+      full_column.each do |cell|
+        cell_states.push(cell.state)
       end
+      if cell_states.include?(".")
+        return true
+      else
+        print "Column full. Make another selection: "
+        return false
+      end
+    end
   end
 
   def input_validation(input) #look at Range
